4eca54303d728ba16845db28f76ef6dbaf19681a
kidell <857463sdc>
2018-07-30 23:45:21 +0800

    at home-0

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserInfo
 
 
 class UserProfileAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_filter = ('phone',)
 
 
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'school', 'company', 'profession', 'address', 'aboutme', 'photo')
+    list_filter = ('school', 'company', 'profession')
+
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserInfo, UserInfoAdmin)
